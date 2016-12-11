820b65174eb6d7676c23d6eab99df12bf314b4f7
Alexander L. Hayes <hayesall@indiana.edu>
2016-12-10 23:59:26 -0500

    fixed unit tests

--- pagerank.py
+++ pagerank.py
@@ -1,28 +1,66 @@
 import pickle
 import networkx as nx
 
+# This is meant to be imported into another function
+# from pagerank import find_pagerank
+
 #I had to review how a pickle dump worked, this is a great place to get started:
 #https://wiki.python.org/moin/UsingPickle
 
 def main():
-    G = nx.from_dict_of_lists(create_test_graph3())
-    pr = pagerank(G)
-    print pr
+    run_unit_tests()
+    #G = nx.from_dict_of_lists(create_test_graph4())
+    #pr = find_pagerank(G)
+    #print pr
     #refer to note at top for pickle files
-    pickle.dump( pr, open("pr.pickle", "wb") )
+    #pickle.dump( pr, open("pr.pickle", "wb") )
 
 def run_unit_tests():
-    G = nx.from_dict_of_lists(create_random_test_graph1())
-    pr = pagerank(G)
+    #G = nx.from_dict_of_lists(create_test_graph1())
+    #pr = find_pagerank(G)
+    pr = find_pagerank(create_test_graph1())
+    test1 = (pr == {'1': 0.29521306459338925,
+                    '3': 0.29521306459338925,
+                    '2': 0.20478693540661067,
+                    '4': 0.20478693540661067})
+    #G = nx.from_dict_of_lists(create_test_graph2())
+    #pr = find_pagerank(G)
+    pr = find_pagerank(create_test_graph2())
+    test2 = (pr == {'1': 0.25,
+                    '3': 0.25,
+                    '2': 0.25,
+                    '4': 0.25})
+    #G = nx.from_dict_of_lists(create_test_graph3())
+    #pr = find_pagerank(G)
+    pr = find_pagerank(create_test_graph3())
+    test3 = (pr == {'1': 0.24025949489415346,
+                    '3': 0.1519481010211692,
+                    '2': 0.1519481010211692,
+                    '5': 0.1519481010211692,
+                    '4': 0.1519481010211692,
+                    '6': 0.1519481010211692})
+    #G = nx.from_dict_of_lists(create_test_graph4())
+    #pr = find_pagerank(G)
+    pr = find_pagerank(create_test_graph4())
+    test4 = (pr == {'1': 0.25, '3': 0.25, '2': 0.25, '4': 0.25})
+    if test1 and test2 and test3 and test4:
+        print 'All tests passed successfully!'
+    else:
+        tests = [test1, test2, test3, test4]
+        for test in range(len(tests)):
+            if not tests[test]:
+                print 'Failed test: %i' % (test + 1)
     
-
 # Useful reference for nxpagerank here:
 #https://networkx.github.io/documentation/networkx-1.10/reference/generated/networkx.algorithms.link_analysis.pagerank_alg.pagerank.html
 # The alpha score is the the random restart value, we discussed that it is typically around 0.85 in class, and sure enough
 # the default value that networkx uses is also 0.85. I include it here to make it explicit.
 
-def pagerank(graph):
-    return nx.pagerank(graph, alpha=0.85)
+def find_pagerank(graph):
+#    import networkx as nx
+    # takes a graph in the form of a python dictionary (refer to the test graphs below)
+    G = nx.from_dict_of_lists(graph)
+    return nx.pagerank(G, alpha=0.85)
 
 def create_test_graph1():
     return {
