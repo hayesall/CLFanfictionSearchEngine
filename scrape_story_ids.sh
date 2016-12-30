e02fd19425ff78317489c854fdb0c4e86b6160a1
TheVanishingMan <alexander@batflyer.net>
2016-12-30 16:48:15 -0500

    Small tweak to allow scraping stories rated M, which were not included in the previous version

--- scrape_story_ids.sh
+++ scrape_story_ids.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-BASE="https://www.fanfiction.net/cartoon/Code-Lyoko/?&srt=1&r=103&p="
+BASE="https://www.fanfiction.net/cartoon/Code-Lyoko/?&srt=1&r=10&p="
 # on the date I wrote this, there were 261 pages of stories
+# update 12/30/2016 changing r=103 to r=10 allows scraping of stories rated M
 
-for i in {1..261}; do
+# update 12/30/2016, 261 -> 285 to include 594 stories that are rated M
+#for i in {1..261}; do
+for i in {1..285}; do
     URL=$BASE$i
     echo "$URL" && echo "$URL" >> sids.txt
     PAGE="`wget --no-check-certificate -q -O - $URL`"
